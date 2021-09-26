c7dd9a8b257a8fc82063bdcd53d2071097ca25c8
cesar <cgorrizm@iesch.org>
2021-09-26 18:16:30 +0200

    Agregar ids a los elementos

--- Practica1/app/src/main/java/com/cesar/practica1/MainActivity.java
+++ Practica1/app/src/main/java/com/cesar/practica1/MainActivity.java
@@ -3,6 +3,8 @@ package com.cesar.practica1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //1- Agregamos los ids a los elementos
+        EditText pripernum = findViewById(R.id.EdPrimerNum);
+        EditText segunnum = findViewById(R.id.EdSegundoNum);
+        EditText resultado = findViewById(R.id.Resultado);
+        Button sumar = findViewById(R.id.btnSumar);
+        Button restar= findViewById(R.id.btnRestar);
+        Button multiplicar= findViewById(R.id.btnMultiplicar);
+        Button dividir= findViewById(R.id.btnDividir);
     }
 }
