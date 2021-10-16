9ac8075d7426e89be8cf5d3ed02d39bff76f5d1a
cesar <cgorrizm@iesch.org>
2021-10-16 16:48:57 +0200

    grafico funcional

--- Practica3/app/src/main/java/com/cesar/practica3/MainActivity.java
+++ Practica3/app/src/main/java/com/cesar/practica3/MainActivity.java
@@ -3,12 +3,36 @@ package com.cesar.practica3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+public int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView numeroPreg = findViewById(R.id.tvNumPre);
+        TextView pregunta = findViewById(R.id.tvPregunta);
+        TextView ecuacion = findViewById(R.id.tvEcuacion);
+        RadioButton primerares = findViewById(R.id.rbRespuesta1);
+        RadioButton segundares= findViewById(R.id.rbRespuesta2);
+        Button btnvalidar = findViewById(R.id.btnValidar);
+
+
+        btnvalidar.setOnClickListener(view -> {
+
+            if (i ==0){
+                if (primerares.isSelected()) {
+                 i++;
+                }else if (segundares.isSelected()){
+
+                }else if (!primerares.isSelected() && !segundares.isSelected()){
+
+                }
+            }
+        });
     }
 }
